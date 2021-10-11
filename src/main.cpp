10ecfbfba5ed93710a4808081119dd0227d610bb
Daniel Soels <daniel.soels@edu.fh-joanneum.at>
2021-10-11 21:04:25 +0200

    FIX I forgot to add these files at the previous commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,40 @@
 #include <Arduino.h>
+#include "Adafruit_Sensor.h"
+#include "DHT.h"
 
-void setup()
+#define DHTPIN D7
+#define DHTTYPE DHT11
+
+#define MEAS_DELAY 1000 // delay in ms
+
+DHT dht(DHTPIN, DHTTYPE);
+
+float act_temp = 0; // last measured temperature
+float act_humid = 0; // last measured humidity
+
+void setup() 
 {
-  // setup here
+  Serial.begin(9600);
+  dht.begin();
 }
 
-void loop()
+void loop() 
 {
-  // loop function
+  static unsigned long last_meas = millis();
+  
+  if((millis() - last_meas) >= MEAS_DELAY)
+  {
+    act_temp = dht.readTemperature();
+    act_humid = dht.readHumidity();
+
+    Serial.print("Temperature: ");
+    Serial.print(act_temp);
+    Serial.println(" °C");
+
+    Serial.print("Humidity: ");
+    Serial.print(act_humid);
+    Serial.println(" %");
+
+    last_meas = millis();
+  }
 }

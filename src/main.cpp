628a5ee14c724b4e6d3d5ab7fd7e2c3aa9c3c90a
JanSloSolo <jan.slovik@edu.fh-joanneum.com>
2021-10-16 15:26:32 +0200

    ADD test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,45 @@
 #include <Arduino.h>
+#include <dht.h>
+#include <Adafruit_Sensor.h>
+
+#define MEAS_DELAY 1000
+#define LED 6
+#define DHTPIN 7
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+float act_temp = 0; // last measured temperature
+float act_humid = 0; // last measured humidity
 
 void setup()
 {
+    pinMode(LED, OUTPUT);
+    Serial.begin(9600);
+    dht.begin();
 }
 
 void loop()
-}
 {
+  digitalWrite(LED, HIGH);
+  delay(500);
+  digitalWrite(LED, LOW);
+  delay(500);
+  static unsigned long last_meas = millis();
+  
+  if((millis() - last_meas) >= MEAS_DELAY)
+  {
+    act_temp = dht.readTemperature();
+    act_humid = dht.readHumidity();
+
+    Serial.print("Temperature: ");
+    Serial.print(act_temp);
+    Serial.println(" °C");
+
+    Serial.print("Humidity: ");
+    Serial.print(act_humid);
+    Serial.println(" %");
+  
+    last_meas = millis();
+  }
+}

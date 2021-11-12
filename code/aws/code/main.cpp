227428fed2d12a8ef1bfd4fdb2c0c652cf79fe51
Tim Schmid <tim.schmid@ise-technology.com>
2021-11-12 09:46:50 +0100

    ADD minor changes

--- code/aws/code/main.cpp
+++ code/aws/code/main.cpp
@@ -10,10 +10,10 @@
 void setup()
 {
   Serial.begin(9600);
-  Serial.println(H_WIFI_PASSWORD H_WIFI_SSID);
+  Serial.println(HOME_PW HOME_SSID);
   Serial.println(xPortGetCoreID());
-  initWIFI(H_WIFI_SSID, H_WIFI_PASSWORD);
-  initAWS(AWS_IOT_ENDPOINT, AWS_IOT_SUBSCRIBE_TOPIC);
+  initWIFI(HOME_SSID, HOME_PW);
+  // initAWS(AWS_IOT_ENDPOINT, AWS_IOT_SUBSCRIBE_TOPIC);
 }
 
 void loop()

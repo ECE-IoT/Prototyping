3e5944afa12b0c8544b934655336b0593d70a452
Tim Schmid <tim.schmid@ise-technology.com>
2021-12-03 18:03:26 +0100

    REF dht22 class

--- code/vertical-prototype/src/sensor/sensor.cpp
+++ code/vertical-prototype/src/sensor/sensor.cpp
@@ -2,71 +2,69 @@
 
 void VEML7700::beginVEML7700()
 {
-    if (!veml.begin())
-    {
-        Serial.println("Sensor not found");
-        while (1)
-            ;
-    }
-    veml.setGain(gain);
-    veml.setIntegrationTime(integration_time);
-    veml.interruptEnable(true);
+  if (!veml.begin())
+  {
+    Serial.println("Sensor not found");
+    while (1)
+      ;
+  }
+  veml.setGain(gain);
+  veml.setIntegrationTime(integration_time);
+  veml.interruptEnable(true);
 }
 
 float VEML7700::readBrightness()
 {
-    float brightness_value = veml.readLux();
-    return brightness_value;
+  float brightness_value = veml.readLux();
+  return brightness_value;
 }
 
 void SoilMoisture::beginSoilMoisture()
 {
-    if (!ss.begin(0x36))
-    {
-        Serial.println("ERROR! seesaw not found");
-        while (1)
-            ;
-    }
-    else
-    {
-        Serial.print("seesaw started! version: ");
-        Serial.println(ss.getVersion(), HEX);
-    }
+  if (!ss.begin(0x36))
+  {
+    Serial.println("ERROR! seesaw not found");
+    while (1)
+      ;
+  }
+  else
+  {
+    Serial.print("seesaw started! version: ");
+    Serial.println(ss.getVersion(), HEX);
+  }
 }
 
 float SoilMoisture::readTemp()
 {
-    float temp_value = ss.getTemp();
-    return temp_value;
+  float temp_value = ss.getTemp();
+  return temp_value;
 }
 
 int SoilMoisture::readMoisture()
 {
-    int capacity = ss.touchRead(0);
-    int moisture_value = map(capacity, soil_moisture_level_low, soil_moisture_level_high, 100, 0);
-    return moisture_value;
+  int capacity       = ss.touchRead(0);
+  int moisture_value = map(capacity, soil_moisture_level_low, soil_moisture_level_high, 100, 0);
+  return moisture_value;
 }
 
-DHT_Sensor::DHT_Sensor(char type, char pin)
+DHT_Sensor::DHT_Sensor(char type, char pin) : dht_(pin, type)
 {
-    dht_type_ = type;
-    dht_pin_ = pin;
-    dht = DHT(dht_pin_, dht_type_);
+  dht_ = DHT(pin, type);
 }
 
 void DHT_Sensor::beginDHT22()
 {
-    dht.begin();
+  dht_.begin();
 }
 
 float DHT_Sensor::readTempDHT22()
 {
-   float temp = dht.readTemperature();
-    return temp;
+  float temp = dht_.readTemperature();
+  return temp;
 }
 
 float DHT_Sensor::readHumDHT22()
 {
-    float hum = dht.readHumidity();
-    return hum;
+  float hum = dht_.readHumidity();
+  return hum;
 }
--- code/vertical-prototype/src/sensor/sensor.h
+++ code/vertical-prototype/src/sensor/sensor.h
@@ -1,49 +1,47 @@
 #ifndef VERT_PROT_SENSOR
 #define VERT_PROT_SENSOR
 
-#include "Arduino.h"
 #include "Adafruit_VEML7700.h"
 #include "Adafruit_seesaw.h"
+#include "Arduino.h"
 #include <DHT.h>
 
-#define gain 10
-#define integration_time 400 // in ms
-#define soil_moisture_level_low 1023
+#define gain                     10
+#define integration_time         400 // in ms
+#define soil_moisture_level_low  1023
 #define soil_moisture_level_high 314
 
 class VEML7700
 {
 private:
-    Adafruit_VEML7700 veml = Adafruit_VEML7700();
+  Adafruit_VEML7700 veml = Adafruit_VEML7700();
 
 public:
-    void beginVEML7700(void);
-    float readBrightness(void);
+  void beginVEML7700(void);
+  float readBrightness(void);
 };
 
 class SoilMoisture
 {
 private:
-    Adafruit_seesaw ss;
+  Adafruit_seesaw ss;
 
 public:
-    void beginSoilMoisture();
-    float readTemp();
-    int readMoisture();
+  void beginSoilMoisture();
+  float readTemp();
+  int readMoisture();
 };
 
 class DHT_Sensor
 {
 private:
-    char dht_type_;
-    char dht_pin_;
-    DHT dht;
+  DHT dht_;
 
 public:
-    DHT_Sensor(char type, char pin);
-    void beginDHT22();
-    float readTempDHT22();
-    float readHumDHT22();
+  DHT_Sensor(char type, char pin);
+  void beginDHT22();
+  float readTempDHT22();
+  float readHumDHT22();
 };
 
 #endif
